3d52b064ff4e635d55dff556693e0c500d69abea
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2021-03-15 16:25:28 +0200

    generate Token for auth & generate admin

--- models/userModel.js
+++ models/userModel.js
@@ -5,8 +5,8 @@
 
 //@All of the above imports have been used. For more information, contact UNIQ CREATIVE SRL Software Department.
 
-import mongoose from 'mongoose'
-import bcrypt from 'bcryptjs'
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema(
     {
